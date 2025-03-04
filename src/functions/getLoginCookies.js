557f3ca5b47305fbfc5c3f6809559684490eecf0
Luiz Silva <luiz.silva@grupozelo.com>
2025-03-04 18:47:36 -0300

    Implmentando

--- src/functions/getLoginCookies.js
+++ src/functions/getLoginCookies.js
@@ -1,14 +1,19 @@
-require('dotenv').config();
-const puppeteer = require('puppeteer');
+require("dotenv").config();
+const puppeteer = require("puppeteer");
 
 async function getLoginCookies() {
-  const browser = await puppeteer.launch({ headless: Boolean(parseInt(process.env.HEADLESS)) });
+  const browser = await puppeteer.launch({
+    headless: Boolean(parseInt(process.env.HEADLESS)),
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   const timeBrowser = process.env.TIME_BROWSER;
 
   try {
     // Acessa a página de login
-    await page.goto(process.env.URL_ZEUS + '/login', { waitUntil: 'networkidle2' });
+    await page.goto(process.env.URL_ZEUS + "/login", {
+      waitUntil: "networkidle2",
+    });
 
     // Preenche os campos de login usando variáveis de ambiente
     await page.type('input[name="username"]', process.env.LOGIN_USER_ZEUS);
@@ -16,8 +21,8 @@ async function getLoginCookies() {
 
     // Clica no botão de login e espera o redirecionamento
     await Promise.all([
-      page.waitForNavigation({ waitUntil: 'networkidle2' }),
-      page.click('button[type="submit"]')
+      page.waitForNavigation({ waitUntil: "networkidle2" }),
+      page.click('button[type="submit"]'),
     ]);
 
     // Obtém os cookies após o login
@@ -25,7 +30,11 @@ async function getLoginCookies() {
     const currentCookie = `PHPSESSID=${cookies[0].value}`;
 
     // Agendar fechamento do navegador sem bloquear a execução
-    console.log(`Navegador permanecerá aberto por ${parseInt(timeBrowser)/60000} minuto(s)...`);
+    console.log(
+      `Navegador permanecerá aberto por ${
+        parseInt(timeBrowser) / 60000
+      } minuto(s)...`
+    );
     setTimeout(async () => {
       console.log("Fechando navegador...");
       await browser.close();
