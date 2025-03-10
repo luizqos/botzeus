74280ae0e6307bea2956a96b3e590eacc9ddf5b1
Luiz Silva <luiz.silva@grupozelo.com>
2025-03-09 22:24:39 -0300

    new args

--- src/functions/getLoginCookies.js
+++ src/functions/getLoginCookies.js
@@ -4,7 +4,15 @@ const puppeteer = require("puppeteer");
 async function getLoginCookies() {
   const browser = await puppeteer.launch({
     headless: Boolean(parseInt(process.env.HEADLESS)),
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--disable-software-rasterizer",
+      "--disable-features=VizDisplayCompositor",
+      "--disable-features=site-per-process"
+    ],
     executablePath: process.env.DIRECTORY_BROWSER,
   });
   const page = await browser.newPage();
